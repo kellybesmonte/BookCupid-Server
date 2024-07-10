b5963ac1ceec012adb6e46a4f25e75efec280bc5
Kelly Anne Besmonte <kellybesmonte@gmail.com>
2024-07-10 14:36:46 -0400

    get book id and get book profile id works, debugging getting genres and quotes

--- server.js
+++ server.js
@@ -1,88 +1,112 @@
 import express from 'express';
-import cors from 'cors'
+import cors from 'cors';
 import 'dotenv/config';
-import mysql from 'mysql2/promise';
-
+import mysql from 'mysql2';
 
 const app = express();
 const PORT = process.env.PORT || 8082;
 const CROSS_ORIGIN = process.env.CROSS_ORIGIN || '*';
 
-app.use(cors({ origin: CROSS_ORIGIN }));
-app.use(express.json());
+let db;
 
 async function initializeDatabase() {
     try {
-    let db = await mysql.createConnection({
-        host: process.env.DB_HOST,
-        user: process.env.DB_LOCAL_USER,
-        password: process.env.DB_LOCAL_PASSWORD,
-        database: process.env.DB_LOCAL_NAME
-    });
+        db = await mysql.createConnection({
+            host: process.env.DB_HOST,
+            user: process.env.DB_LOCAL_USER,
+            password: process.env.DB_LOCAL_PASSWORD,
+            database: process.env.DB_LOCAL_NAME
+        });
 
-    console.log('Connected to the database');
+        console.log('Connected to the database');
     } catch (err) {
-    console.error('Error connecting to the database:', err);
+        console.error('Error connecting to the database:', err);
     }
-    }
+}
 
-    initializeDatabase();
+initializeDatabase(); 
+
+app.use(cors({ origin: CROSS_ORIGIN }));
+app.use(express.json());
+
+// ROUTES //
 
 app.get('/', (req, res) => {
     res.send('Book Cupid');
 });
 
-///GET GENRE 
-app.get('/genres', (req, res) => {
-    db.query('SELECT * FROM genres', (err, results) => {
-    if (err) {
-        res.status(500).send(err);
-    } else {
-        res.json(results);
-    }
-    });
-});
+// GET GENRES
+// app.get('/genres', (req, res) => {
+//     if (!db) {
+//         res.status(500).send('Database connection not established');
+//         return;
+//     }
 
-//GET QUOTES BY GENRE
+//     db.query('SELECT * FROM genres', (err, results) => {
+//         if (err) {
+//             res.status(500).send(err);
+//         } else {
+//             res.json(results);
+//         }
+//     });
+// });
+
+// GET QUOTES BY GENRE
 app.get('/quotes/:genre', (req, res) => {
+    if (!db) {
+        res.status(500).send('Database connection not established');
+        return;
+    }
+
     const genre = req.params.genre;
     db.query('SELECT * FROM quotes WHERE genre = ?', [genre], (err, results) => {
-    if (err) {
-        res.status(500).send(err);
-    } else {
-        res.json(results);
-    }
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(results);
+        }
     });
 });
 
-//GET BOOK PROFILE BY ID
+// GET BOOK BY ID
 app.get('/books/:id', (req, res) => {
+    if (!db) {
+        res.status(500).send('Database connection not established');
+        return;
+    }
+
     const id = req.params.id;
     db.query('SELECT * FROM books WHERE id = ?', [id], (err, results) => {
-    if (err) {
-        res.status(500).send(err);
-    } else {
-        res.json(results);
-    }
+        if (err) {
+            res.status(500).send(err);
+        } else {
+            res.json(results);
+        }
     });
 });
 
-//GET STRUCTURED BOOK DESCRIPTION
+// GET STRUCTURED BOOK DESCRIPTION
 app.get('/book_profiles/:id', (req, res) => {
+    if (!db) {
+        res.status(500).send('Database connection not established');
+        return;
+    }
+
     const id = req.params.id;
     db.query(`
         SELECT b.title, b.author, bp.structured_description
         FROM books b
         JOIN book_profiles bp ON b.id = bp.book_id
         WHERE b.id = ?`, [id], (err, results) => {
-        if (err) {
-        res.status(500).send(err);
-    } else {
-    res.json(results);
-    }
-    });
+            if (err) {
+                res.status(500).send(err);
+            } else {
+                res.json(results);
+            }
+        });
 });
 
 app.listen(PORT, () => {
     console.log(`App is running on port ${PORT}`);
 });
+
