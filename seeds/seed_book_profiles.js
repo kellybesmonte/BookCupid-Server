641b7a7c97b90f62814694d9517939a968deef55
Kelly Anne Besmonte <kellybesmonte@gmail.com>
2024-07-18 09:26:08 -0400

    updated seeds

--- seeds/seed_book_profiles.js
+++ seeds/seed_book_profiles.js
@@ -1,19 +1,14 @@
 /**
-
  * 
  * @param { import("knex").Knex } knex 
  * @returns { Promise<void> }
  */
 export async function seed(knex) {
+  try {
 
-  const existingBookProfiles = await knex("book_profiles").select("*");
-
-
-  if (existingBookProfiles.length === 0) {
-
-      const bookProfilesData = await knex("existing_book_profiles").select("book_id", "description");
-
-      await knex("book_profiles").insert(bookProfilesData);
+    const bookProfilesData = await knex("books").select("id", "title", "author", "description", "class", "genre", "link");
+    console.log("Data fetched successfully from books:", bookProfilesData);
+  } catch (error) {
+    console.error("Error fetching data from books:", error);
   }
 }
-
