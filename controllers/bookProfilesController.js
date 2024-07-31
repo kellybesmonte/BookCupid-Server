8ea5889cdfa44dab7a768ab215da00d5bbce4533
Kelly Anne Besmonte <kellybesmonte@gmail.com>
2024-07-31 16:43:00 -0400

    simplified router code

--- controllers/bookProfilesController.js
+++ controllers/bookProfilesController.js
@@ -11,17 +11,10 @@ router.get('/book_profiles/genre/:genres', async (req, res) => {
         const genres = req.params.genres.split(',').map(genre => genre.trim());
 
 
-        const genreConditions = genres.map(genre => 
-            `JSON_CONTAINS(b.genre, '["${genre}"]', '$')`
-        ).join(' OR ');
-
-
         const results = await db('book_profiles as bp')
             .join('books as b', 'bp.book_id', 'b.id')
-            .whereRaw(`(${genreConditions})`)
-            .select('bp.*', 'b.genre');
-
-            console.log('Query results:', results);
+            .whereRaw('JSON_CONTAINS(b.genre, ?, ?)', [JSON.stringify(genres), '$'])
+            .select('bp.id', 'bp.book_id', 'b.genre');
 
         if (results.length === 0) {
             res.status(404).send('No book profiles found for these genres');
